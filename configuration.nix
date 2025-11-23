11713d0def46752315698ee8634c67231a4331d7
David Nobles <david.nobles@sontric.org>
2025-11-22 21:42:04 -0800

    add tailscale

--- configuration.nix
+++ configuration.nix
@@ -36,6 +36,9 @@
   # You can disable this if you're only using the Wayland session.
   services.xserver.enable = true;
 
+  # Tailscale for networking
+  services.tailscale.enable = true;
+
   # Enable the KDE Plasma Desktop Environment.
   services.displayManager.sddm.enable = true;
   services.desktopManager.plasma6.enable = true;
